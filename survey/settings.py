4ad351056f8c2be30d35b9a3e716934b3b20c237
Ira W. Snyder <isnyder@lco.global>
2019-06-25 11:14:46 -0700

    Configure for PostgreSQL database
    This project will use the shared PostgreSQL database server, just like
    other projects.

--- survey/settings.py
+++ survey/settings.py
@@ -76,14 +76,13 @@ WSGI_APPLICATION = 'survey.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ.get('DB_HOST', ''),
         'NAME': os.environ.get('DB_NAME', ''),
-        "USER": os.environ.get('DB_USER', ''),
-        "PASSWORD": os.environ.get('DB_PASS', ''),
-        "HOST": os.environ.get('DB_HOST', ''),
-        "ENGINE": "django.db.backends.mysql",
-        "PORT"   : '3306',
-        }
+        'USER': os.environ.get('DB_USER', ''),
+        'PASSWORD': os.environ.get('DB_PASS', ''),
     }
+}
 
 
 # Password validation
