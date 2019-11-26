37d4cf37d134651f4c558e4155243b61a3282250
Edward Gomez <edward@gomez.me.uk>
2019-11-26 10:48:40 +0000

    Removing duplicate footer

--- polls/views.py
+++ polls/views.py
@@ -32,13 +32,20 @@ class DetailView(generic.DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         # Add in a QuerySet of all the questions
-        answers = self.object.choice_set.all().values('question_id','question__text','choice','id')
+        questions = Survey.objects.all()[0].choice_set.all().values('survey','question_id','question__text').distinct()
         qs = []
-        for a in answers:
-            tmpq = a
-            tmpq['icon'] = LIKERT_ICONS[a['choice']]
-            tmpq['text'] = dict(LIKERT_CHOICES)[a['choice']]
-            qs.append(tmpq)
+        lk_choices = dict(LIKERT_CHOICES)
+
+        for q in questions:
+            tmp_q = dict(q)
+            choices = list(Choice.objects.filter(question=q['question_id'],survey=q['survey']).values_list('choice','id').order_by('choice').distinct())
+            c_dict = {cs[0]:cs[1]for cs in choices}
+            ch_tmp = []
+            for ch in choices:
+                ch_tmp.append({'text' : lk_choices[ch[0]], 'id':ch[1],'icon':LIKERT_ICONS[ch[0]]})
+            tmp_q['choices'] = ch_tmp
+            qs.append(tmp_q)
+        context['questions'] = qs
         context['questions'] = qs
         cookie = self.request.COOKIES.get(self.object.cookie_name, None)
         if cookie and cookie == str(self.object.id):
