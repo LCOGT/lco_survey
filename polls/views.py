50a5c4493928dd1be5b2d720e4b0c0f42431aea0
Edward Gomez <edward@gomez.me.uk>
2019-11-26 10:30:16 +0000

    Remove comments

--- polls/views.py
+++ polls/views.py
@@ -32,21 +32,6 @@ class DetailView(generic.DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         # Add in a QuerySet of all the questions
-        # questions = self.object.choice_set.all().values('id','question_id','question__text')
-        # questions = Survey.objects.all()[0].choice_set.all().values('survey','question_id','question__text').distinct()
-        # qs = []
-        # lk_choices = dict(LIKERT_CHOICES)
-        #
-        # for q in questions:
-        #     tmp_q = dict(q)
-        #     choices = list(Choice.objects.filter(question=q['question_id'],survey=q['survey']).values_list('choice','id').order_by('choice').distinct())
-        #     c_dict = {cs[0]:cs[1]for cs in choices}
-        #     ch_tmp = []
-        #     for ch in choices:
-        #         ch_tmp.append({'text' : lk_choices[ch[0]], 'id':ch[1],'icon':LIKERT_ICONS[ch[0]]})
-        #     tmp_q['choices'] = ch_tmp
-        #     qs.append(tmp_q)
-        # context['questions'] = qs
         answers = self.object.choice_set.all().values('question_id','question__text','choice','id')
         qs = []
         for a in answers:
